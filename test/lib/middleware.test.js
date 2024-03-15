801919429d08d91f2ee9980bbb29b2a479feb5d7
Zsolt Tövis <tovis.zsolt@gmail.com>
2024-03-15 12:34:11 +0100

    update middleware

---
+++ test/lib/middleware.test.js
@@ -0,0 +1,50 @@
+const { registerMiddleware, executeMiddleware } = require('../../lib/middleware');
+
+let mockMiddlewareFn;
+
+beforeEach(() => {
+  jest.resetModules();
+  mockMiddlewareFn = jest.fn((res, next) => {
+    // console.log('Executing mock middleware...');
+    next();
+  });
+});
+
+describe('executeMiddleware', () => {
+  it('Single method', async () => {
+    const res = {};
+    registerMiddleware('pre', 'products', 'create', mockMiddlewareFn);
+    await executeMiddleware('pre', 'products', 'create', res);
+
+    expect(mockMiddlewareFn).toHaveBeenCalled();
+    expect(mockMiddlewareFn).toHaveBeenCalledTimes(1);
+  });
+
+  it('Multiple methods', async () => {
+    const res = {};
+    registerMiddleware('pre', 'products', ['create', 'update'], mockMiddlewareFn);
+    await executeMiddleware('pre', 'products', 'create', res);
+    await executeMiddleware('pre', 'products', 'update', res);
+
+    expect(mockMiddlewareFn).toHaveBeenCalled();
+    expect(mockMiddlewareFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('Invalid middleware type', async () => {
+    const res = {};
+    registerMiddleware('invalid', 'products', 'create', mockMiddlewareFn);
+    await executeMiddleware('pre', 'products', 'create', res);
+
+    expect(mockMiddlewareFn).not.toHaveBeenCalled();
+    expect(mockMiddlewareFn).toHaveBeenCalledTimes(0);
+  });
+
+  it('Non-exist middleware', async () => {
+    const res = {};
+    registerMiddleware('pre', 'products', 'create', mockMiddlewareFn);
+    await executeMiddleware('post', 'products', 'create', res);
+
+    expect(mockMiddlewareFn).not.toHaveBeenCalled();
+    expect(mockMiddlewareFn).toHaveBeenCalledTimes(0);
+  });
+});
