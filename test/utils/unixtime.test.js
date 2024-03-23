2f8a77069609581e804f42eab44dcce99f5413b1
Zsolt Tövis <tovis.zsolt@gmail.com>
2024-03-23 03:09:43 +0100

    fix date based tests

--- test/utils/unixtime.test.js
+++ test/utils/unixtime.test.js
@@ -1,3 +1,4 @@
+const moment = require('moment-timezone');
 const { timeNow, timeFromStr, timeToDateObj, timeToDateStr } = require('../../utils/unixtime');
 
 describe('timeNow', () => {
@@ -9,7 +10,7 @@ describe('timeNow', () => {
 
 describe('timeFromStr', () => {
   it('convert a date string to Unix time format', () => {
-    const dateStr = '2024-02-28T12:00:00Z'; // Assuming UTC time zone
+    const dateStr = '2024-02-28T12:00:00Z';
     const expectedTime = Math.floor(new Date(dateStr).getTime() / 1000);
     expect(timeFromStr(dateStr)).toBe(expectedTime);
   });
@@ -17,7 +18,7 @@ describe('timeFromStr', () => {
 
 describe('timeToDateObj', () => {
   it('convert Unix time format to a Date object', () => {
-    const unixTime = 1646044800; // Unix time for '2022-02-28T10:40:00Z'
+    const unixTime = 1646044800;
     const expectedDate = new Date('2022-02-28T10:40:00Z');
     expect(timeToDateObj(unixTime)).toEqual(expectedDate);
   });
@@ -25,15 +26,18 @@ describe('timeToDateObj', () => {
 
 describe('timeToDateStr', () => {
   it('convert Unix time format to a date string with default format', () => {
-    const unixTime = 1646052000; // Unix time for '2022-02-28T13:40:00Z'
-    const expectedDateString = '2022-02-28 13:40:00'; // Local time
-    expect(timeToDateStr(unixTime)).toBe(expectedDateString);
+    const unixTime = 1646052000;
+    const expectedDate = moment.unix(unixTime).format('YYYY-MM-DD HH:mm:ss');
+    expect(timeToDateStr(unixTime)).toBe(expectedDate);
   });
 
   it('convert Unix time format to a date string with custom format', () => {
-    const unixTime = 1646044800; // Unix time for '2022-02-28T10:40:00Z'
+    const unixTime = 1646044800;
     const customFormat = '%Y-%M-%D %H:%I:%S %MER';
-    const expectedDateString = '2022-02-28 11:40:00 A.M.'; // Local time
-    expect(timeToDateStr(unixTime, customFormat)).toBe(expectedDateString);
+    const expextedDate = moment
+      .unix(unixTime)
+      .format('YYYY-MM-DD HH:mm:ss A')
+      .replace(/(A|P)M/, '$1.M.');
+    expect(timeToDateStr(unixTime, customFormat)).toBe(expextedDate);
   });
 });
