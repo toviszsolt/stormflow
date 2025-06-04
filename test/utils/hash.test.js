0c17990425aa661c16523bc6008970533f3dd6f4
Zsolt Tovis <tovis.zsolt@gmail.com>
2025-06-04 23:42:42 +0200

    extend uniqueId tests

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -11,5 +11,23 @@ describe('uniqueId', () => {
     const id = uniqueId();
     const regex = /^[0-9a-f]{12}$/;
     expect(id).toMatch(regex);
+    expect(id).toBe(id.toLowerCase());
+  });
+
+  it('generates 1000 unique ids', () => {
+    const ids = new Set();
+    for (let i = 0; i < 1000; i++) {
+      ids.add(uniqueId());
+    }
+    expect(ids.size).toBe(1000);
+  });
+
+  it('generates ids efficiently', () => {
+    const start = Date.now();
+    for (let i = 0; i < 10000; i++) {
+      uniqueId();
+    }
+    const duration = Date.now() - start;
+    expect(duration).toBeLessThan(500); // 10k ID < 0.5s legyen
   });
 });
