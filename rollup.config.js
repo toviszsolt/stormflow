8c26321c757f721abda6640ad159086c3022fc80
Zsolt Tovis <tovis.zsolt@gmail.com>
2025-06-20 04:38:20 +0200

    add rollup-plugin-delete

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,15 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
+import del from 'rollup-plugin-delete';
 import { terser } from 'rollup-plugin-terser';
 
 const dist = './dist';
 const terserOptions = { format: { comments: false } };
+const outputOptions = { generatedCode: 'es2015' };
 
 const plugins = [
+  del({ targets: dist, runOnce: true, verbose: true }),
   resolve({ preferBuiltins: true }),
   commonjs(),
   json(),
@@ -25,24 +28,24 @@ export default [
   {
     input: 'src/stormflow.js',
     output: [
-      { file: `${dist}/stormflow.js`, format: 'es', generatedCode: 'es2015' },
-      { file: `${dist}/stormflow.cjs`, format: 'cjs', generatedCode: 'es2015' },
+      { file: `${dist}/stormflow.js`, format: 'es', ...outputOptions },
+      { file: `${dist}/stormflow.cjs`, format: 'cjs', ...outputOptions },
     ],
     plugins,
   },
   {
     input: 'src/storage/fileStorageAdapter.js',
     output: [
-      { file: `${dist}/adapters/fileStorageAdapter.js`, format: 'es', generatedCode: 'es2015' },
-      { file: `${dist}/adapters/fileStorageAdapter.cjs`, format: 'cjs', generatedCode: 'es2015' },
+      { file: `${dist}/adapters/fileStorageAdapter.js`, format: 'es', ...outputOptions },
+      { file: `${dist}/adapters/fileStorageAdapter.cjs`, format: 'cjs', ...outputOptions },
     ],
     plugins,
   },
   {
     input: 'src/storage/fileBackupAdapter.js',
     output: [
-      { file: `${dist}/adapters/fileBackupAdapter.js`, format: 'es', generatedCode: 'es2015' },
-      { file: `${dist}/adapters/fileBackupAdapter.cjs`, format: 'cjs', generatedCode: 'es2015' },
+      { file: `${dist}/adapters/fileBackupAdapter.js`, format: 'es', ...outputOptions },
+      { file: `${dist}/adapters/fileBackupAdapter.cjs`, format: 'cjs', ...outputOptions },
     ],
     plugins,
   },
